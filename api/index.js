8f5fef3cef27420a7620c482b970b56c67ef4abc
Vedant Tathe <tathevedant70@gmail.com>
2025-06-20 14:24:34 +0530

    commited

--- api/index.js
+++
@@ -1,4 +0,0 @@
-const app = require('../index'); // or './index' if index.js is in root
-const serverless = require('serverless-http');
-
-module.exports = serverless(app);
--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const urlRoutes = require('./routes/urlRoutes'); 
+const urlRoutes = require('./routes/urlRoutes');
+const serverless = require('serverless-http');
 
 dotenv.config();
+
 const app = express();
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB connected"))
+  .then(() => console.log("MongoDB connected"))
   .catch(err => console.log("DB error:", err));
 
-app.use('/', urlRoutes); 
+app.use('/api', urlRoutes); 
 
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running...`));
-
-
-const serverless = require('serverless-http');
-
-module.exports = serverless(app);
+module.exports = app;          
+module.exports.handler = serverless(app); 
